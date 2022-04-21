722beae22ccc827f2dd572919e2d7e05448825f4
BlaneTech <falloud70gmail.com>
2022-04-21 01:02:32 +0000

    modif page form ajout and finish page listUser

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+# @app.route('/')
 @app.route('/pagePrincipal')
 def pagePrincipal():
     return render_template('pagePrincipal.html')

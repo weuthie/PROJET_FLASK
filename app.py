fef687412bfa4ac81200de5f16160263e8dcca7d
Ousseynou Diop <ouzediop1234@gmail.com>
2022-04-21 08:33:13 +0000

    a revoir

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for ,request
 
 app = Flask(__name__)
 
@@ -6,3 +6,11 @@ app = Flask(__name__)
 @app.route('/pagePrincipal')
 def pagePrincipal():
     return render_template('pagePrincipal.html')
+
+@app.route('/adduser', methods=['get','POST'])
+def adduser():
+    name = request.form['name']
+    username = request.form['username']
+
+
+app.run()
